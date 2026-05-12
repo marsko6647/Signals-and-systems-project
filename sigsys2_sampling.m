c40d92183edd5337c057cdcbeb26c560420c60af
Markus Sköld <marsko6647@gmail.com>
2026-05-12 11:36:30 +0200

    Add files via upload

--- sigsys2_sampling.m
+++ sigsys2_sampling.m
@@ -1,10 +1,13 @@
 
-fc = 8e3;   % till filtret
+fc = 8e3;   % till filtren
 Wn = 2*pi*fc;
+Rp = 3;
 
 fs = 24000;             % samplingsfrekvens
-t_kont = 0:1e-5:10e-3;   
-t_dis = 0:1/fs:10e-3;
+t_kont = 0:1e-5:25e-3;  % första 25ms
+t_dis = 0:1/fs:25e-3;
+w = logspace(1,6,1000);   % rad/s
+
 
 % intressanta signaler
 x1 = 1*sin(2*pi*1000*t_kont);   % 1 (kHz)
@@ -13,13 +16,14 @@ x3 = 1*sin(2*pi*8000*t_kont);  % 8
 
 % störningar
 n1 = 1*sin(2*pi*11000*t_kont);   % 11
-n2 = 1*sin(2*pi*18000*t_kont); % 18 
+n2 = 1*sin(2*pi*12000*t_kont); % 18 
 
 % analog insignal
-x = n1;
+x = n2;
 
 % filter
-[b, a] = butter(12, Wn, "s");
+% [b, a] = butter(12, Wn,'s');
+[b, a] = cheby1(8, Rp, Wn, 's');
 
 sys = tf(b, a);
 
@@ -29,27 +33,32 @@ y = lsim(sys, x, t_kont);
 % diskret utsignal kollar vad y har för värden vid t_dis
 y_dis = interp1(t_kont, y, t_dis);
 
+% kolla ripple
+figure
+bode(sys)
+grid on
+
 figure
 subplot(1,3,1)
 plot(t_kont, x);
 title('insignal')
 xlabel('t (s)')
 ylabel('U (V)')
-axis([0 2e-3 -1 1])
+axis([0 0.5e-3 -1 1])
 
 subplot(1,3,2)
 plot(t_kont,y);
 title('utsignal kontinuerlig')
 xlabel('t (s)')
 ylabel('U (V)')
-axis([0 2e-3 -1 1])
+axis([0 5e-3 -1 1])
 
 subplot(1,3,3)
 stem(t_dis, y_dis);
 title('utsignal diskret')
 xlabel('t (s)')
 ylabel('U (V)')
-axis([0 2e-3 -1 1])
+axis([0 5e-3 -1 1])
 
 
 
