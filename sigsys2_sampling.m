3d4f857409fc85c52bd908a99c1b0b099dc77780
Markus Sköld <marsko6647@gmail.com>
2026-05-12 10:56:20 +0200

    Add files via upload

---
+++ sigsys2_sampling.m
@@ -0,0 +1,56 @@
+
+fc = 8e3;   % till filtret
+Wn = 2*pi*fc;
+
+fs = 24000;             % samplingsfrekvens
+t_kont = 0:1e-5:10e-3;   
+t_dis = 0:1/fs:10e-3;
+
+% intressanta signaler
+x1 = 1*sin(2*pi*1000*t_kont);   % 1 (kHz)
+x2 = 1*sin(2*pi*5000*t_kont);  % 5 
+x3 = 1*sin(2*pi*8000*t_kont);  % 8 
+
+% störningar
+n1 = 1*sin(2*pi*11000*t_kont);   % 11
+n2 = 1*sin(2*pi*18000*t_kont); % 18 
+
+% analog insignal
+x = n1;
+
+% filter
+[b, a] = butter(12, Wn, "s");
+
+sys = tf(b, a);
+
+% analog utsignal
+y = lsim(sys, x, t_kont);
+
+% diskret utsignal kollar vad y har för värden vid t_dis
+y_dis = interp1(t_kont, y, t_dis);
+
+figure
+subplot(1,3,1)
+plot(t_kont, x);
+title('insignal')
+xlabel('t (s)')
+ylabel('U (V)')
+axis([0 2e-3 -1 1])
+
+subplot(1,3,2)
+plot(t_kont,y);
+title('utsignal kontinuerlig')
+xlabel('t (s)')
+ylabel('U (V)')
+axis([0 2e-3 -1 1])
+
+subplot(1,3,3)
+stem(t_dis, y_dis);
+title('utsignal diskret')
+xlabel('t (s)')
+ylabel('U (V)')
+axis([0 2e-3 -1 1])
+
+
+
+
